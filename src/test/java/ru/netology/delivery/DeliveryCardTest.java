6676e2a6237b45703a62a8e00b336d95ac2043cc
Larisa Pylaeva <laralord@yandex.ru>
2021-06-25 14:22:30 +0300

    First commit

---
+++ src/test/java/ru/netology/delivery/DeliveryCardTest.java
@@ -0,0 +1,20 @@
+package ru.netology.delivery;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class DeliveryCardTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+    @Test
+            void shouldTestSomething() {
+
+
+    }
+
+}
